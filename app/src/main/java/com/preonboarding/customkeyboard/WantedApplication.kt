08ef48180769d32f0eee77618d2c78c9409f1466
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-10-12 13:14:22 +0900

    feat - 애플리케이션 클래스 추가

---
+++ app/src/main/java/com/preonboarding/customkeyboard/WantedApplication.kt
@@ -0,0 +1,9 @@
+package com.preonboarding.customkeyboard
+
+import android.app.Application
+
+class WantedApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
