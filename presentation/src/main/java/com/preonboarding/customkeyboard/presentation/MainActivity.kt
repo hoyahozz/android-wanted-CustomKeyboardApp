b75b45e7df01fcdb6edfec1cd66a4df635f4f5ff
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-10-12 13:20:10 +0900

    chore - Timber 의존성 추가

--- presentation/src/main/java/com/preonboarding/customkeyboard/presentation/MainActivity.kt
+++ presentation/src/main/java/com/preonboarding/customkeyboard/presentation/MainActivity.kt
@@ -2,7 +2,6 @@ package com.preonboarding.customkeyboard.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.preonboarding.customkeyboard.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
