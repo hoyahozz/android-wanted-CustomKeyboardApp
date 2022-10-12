cb4f31def417e6c02aa0c1477f3a06aa248f8608
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-10-12 13:42:10 +0900

    chore - Hilt 의존성 추가

--- app/src/main/java/com/preonboarding/customkeyboard/WantedApplication.kt
+++ app/src/main/java/com/preonboarding/customkeyboard/WantedApplication.kt
@@ -1,8 +1,10 @@
 package com.preonboarding.customkeyboard
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
+@HiltAndroidApp
 class WantedApplication : Application() {
     override fun onCreate() {
         super.onCreate()
