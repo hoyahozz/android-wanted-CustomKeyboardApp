48aeddd712bd85180d3d450a1789770a0cd9e141
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-10-12 13:20:24 +0900

    feat - Timber Plant 추가

--- app/src/main/java/com/preonboarding/customkeyboard/WantedApplication.kt
+++ app/src/main/java/com/preonboarding/customkeyboard/WantedApplication.kt
@@ -1,9 +1,16 @@
 package com.preonboarding.customkeyboard
 
 import android.app.Application
+import timber.log.Timber
 
 class WantedApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        Timber.plant(object : Timber.DebugTree() {
+            override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+                super.log(priority, "Debug[$tag]", message, t)
+            }
+        })
     }
 }
