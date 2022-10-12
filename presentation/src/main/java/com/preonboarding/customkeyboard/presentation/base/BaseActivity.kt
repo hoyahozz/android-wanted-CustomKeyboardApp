4b147ec36002fef4cd33e661c6362d6f579bd99a
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-10-12 15:50:40 +0900

    feat - BaseActivity, BaseFragment 생성

---
+++ presentation/src/main/java/com/preonboarding/customkeyboard/presentation/base/BaseActivity.kt
@@ -0,0 +1,19 @@
+package com.preonboarding.customkeyboard.presentation.base
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<T : ViewDataBinding> : AppCompatActivity() {
+
+    lateinit var binding: T
+    abstract val layoutResourceId: Int
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutResourceId)
+        binding.lifecycleOwner = this
+        setContentView(binding.root)
+    }
+}
