1c5fc02a7f385b604b225ab060bad0637b599189
gyurim <gyurim1250@gmail.com>
2022-10-13 23:44:36 +0900

    feat: inputConnection 의존성 주입

--- presentation/src/main/java/com/preonboarding/customkeyboard/presentation/keyboard/KoreaLanguageMaker.kt
+++ presentation/src/main/java/com/preonboarding/customkeyboard/presentation/keyboard/KoreaLanguageMaker.kt
@@ -1,15 +1,11 @@
 package com.preonboarding.customkeyboard.presentation.keyboard
 
 import android.view.inputmethod.InputConnection
+import javax.inject.Inject
 
-class KoreaLanguageMaker {
-
-    private lateinit var inputConnection: InputConnection
-
-    constructor(inputConnection: InputConnection) {
-        this.inputConnection = inputConnection
-    }
-
+class KoreaLanguageMaker @Inject constructor(
+    private val inputConnection: InputConnection
+){
     open fun commit(c: Char){
         directlyCommit()
     }
