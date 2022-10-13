4828a00263cb02a0e80cc9bdb806e60063965ac5
sujin-kk <sjoo0002@gmail.com>
2022-10-14 03:23:20 +0900

    feat - toolbar 추가해서 프래그먼트 전환 구현

--- presentation/src/main/java/com/preonboarding/customkeyboard/presentation/ui/input/InputActivity.kt
+++ presentation/src/main/java/com/preonboarding/customkeyboard/presentation/ui/input/InputActivity.kt
@@ -2,9 +2,13 @@ package com.preonboarding.customkeyboard.presentation.ui.input
 
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.fragment.app.Fragment
+import com.preonboarding.customkeyboard.domain.model.ToolbarMenu
 import com.preonboarding.customkeyboard.presentation.R
 import com.preonboarding.customkeyboard.presentation.common.base.BaseActivity
 import com.preonboarding.customkeyboard.presentation.databinding.ActivityInputBinding
+import com.preonboarding.customkeyboard.presentation.ui.input.clipboard.ClipboardFragment
+import com.preonboarding.customkeyboard.presentation.ui.input.keyboard.KeyboardFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,6 +19,66 @@ class InputActivity : BaseActivity<ActivityInputBinding>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        bindingView()
+        initToolbar()
+    }
+
+    private fun bindingView() {
+        viewModel.toolbarMenu.observe(this) {
+            binding.layoutToolbar.toolbarMenu = it
+
+            when(it) {
+                ToolbarMenu.KEYBOARD -> {
+                    replaceFragment(KeyboardFragment())
+                }
+
+                ToolbarMenu.CLIPBOARD -> {
+                    replaceFragment(ClipboardFragment())
+                }
+
+                else -> {
+                    replaceFragment(KeyboardFragment())
+                }
+            }
+        }
+    }
+
+    private fun initToolbar() {
+
+        binding.layoutToolbar.apply {
+            layoutKeyboardTab.setOnClickListener {
+                viewModel.changeMenu()
+                changeFragment()
+            }
+
+            layoutClipboardTab.setOnClickListener {
+                viewModel.changeMenu()
+                changeFragment()
+            }
+
+        }
+    }
+
+    private fun changeFragment() {
+        when(viewModel.toolbarMenu.value) {
+            ToolbarMenu.KEYBOARD -> {
+                replaceFragment(KeyboardFragment())
+            }
+
+            ToolbarMenu.CLIPBOARD -> {
+                replaceFragment(ClipboardFragment())
+            }
+
+            else -> {
+                replaceFragment(KeyboardFragment())
+            }
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fl_fragment_container, fragment)
+            .commitAllowingStateLoss()
     }
 
 }
--- presentation/src/main/java/com/preonboarding/customkeyboard/presentation/ui/input/InputViewModel.kt
+++ presentation/src/main/java/com/preonboarding/customkeyboard/presentation/ui/input/InputViewModel.kt
@@ -2,7 +2,85 @@ package com.preonboarding.customkeyboard.presentation.ui.input
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.preonboarding.customkeyboard.domain.model.Bookmark
+import com.preonboarding.customkeyboard.domain.model.ToolbarMenu
+import com.preonboarding.customkeyboard.domain.usecase.DeleteBookmarkUseCase
+import com.preonboarding.customkeyboard.domain.usecase.GetBookmarkListUseCase
+import com.preonboarding.customkeyboard.domain.usecase.SaveBookmarkUseCase
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import javax.inject.Inject
 
 @HiltViewModel
-class InputViewModel: ViewModel() {
+class InputViewModel @Inject constructor(
+    private val getBookmarkListUseCase: GetBookmarkListUseCase,
+    private val saveBookmarkUseCase: SaveBookmarkUseCase,
+    private val deleteBookmarkUseCase: DeleteBookmarkUseCase,
+) : ViewModel() {
+    private var localBookmarkList = mutableListOf<Bookmark>()
+
+    // 북마크 (복사된 단어) 리스트
+    private val _toolbarMenu = MutableLiveData<ToolbarMenu>(ToolbarMenu.KEYBOARD)
+    val toolbarMenu: LiveData<ToolbarMenu> get() = _toolbarMenu
+
+    // 북마크 (복사된 단어) 리스트
+    private val _localBookmarks = MutableLiveData<List<Bookmark>>()
+    val localBookmarks: LiveData<List<Bookmark>> get() = _localBookmarks
+
+    init {
+        // localBookmark 변수 초기화 이후에 init block 호출
+        getBookmarks()
+    }
+
+    private fun getBookmarks() {
+        kotlin.runCatching {
+            viewModelScope.launch {
+                localBookmarkList = getBookmarkListUseCase.invoke().toMutableList()
+                _localBookmarks.postValue(localBookmarkList.toList())
+            }
+        }
+            .onSuccess {
+                Timber.tag("TAG").e("success get bookmarks")
+            }
+    }
+
+    fun saveBookmark(bookmark: Bookmark) {
+        kotlin.runCatching {
+            viewModelScope.launch {
+                saveBookmarkUseCase.invoke(bookmark = bookmark)
+            }
+        }
+            .onSuccess {
+                localBookmarkList.add(bookmark)
+                _localBookmarks.setValue(localBookmarkList.toList())
+            }
+    }
+
+    fun deleteBookmark(bookmark: Bookmark) {
+        kotlin.runCatching {
+            viewModelScope.launch {
+                deleteBookmarkUseCase.invoke(bookmark = bookmark)
+            }
+        }
+            .onSuccess {
+                localBookmarkList.remove(bookmark)
+                _localBookmarks.setValue(localBookmarkList.toList())
+            }
+    }
+
+    fun changeMenu() {
+        if (_toolbarMenu.value == ToolbarMenu.KEYBOARD) {
+            _toolbarMenu.value = ToolbarMenu.CLIPBOARD
+        }
+        else {
+            _toolbarMenu.value = ToolbarMenu.KEYBOARD
+        }
+    }
+
+    companion object {
+        private const val TAG = "InputViewModel"
+    }
 }
